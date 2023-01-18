4256427b81fb2f36d62b7e2a5f7d979e4fb47cbb
Kirill Nazarov <deeplogic2000@gmail.com>
2023-01-19 00:01:24 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import './App.css';
+
+const App = () => {
+  
+  return (
+    <div className="App">
+      Hello World!!!!!
+
+    </div>
+  );
+}
+
+export default App;
